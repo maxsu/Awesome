eada75fecee82055eb024906ae68d04c40447cc9
Julien Danjou <julien@danjou.info>
2009-10-13 17:01:12 +0200

    window: import banned property
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- objects/client.h
+++ objects/client.h
@@ -88,10 +88,6 @@ struct client_t
     bool modal;
     /** True if the client is on top */
     bool ontop;
-    /** True if a client is banned to a position outside the viewport.
-     * Note that the geometry remains unchanged and that the window is still mapped.
-     */
-    bool isbanned;
     /** true if the client must be skipped from task bar client list */
     bool skip_taskbar;
     /** True if the client cannot have focus */
