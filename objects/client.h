7bbb2b44d9a6c53ff53235c3a4a7f157af787141
Julien Danjou <julien@danjou.info>
2009-10-13 17:36:07 +0200

    client: client_set_focus is static, so merge
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- objects/client.h
+++ objects/client.h
@@ -163,7 +163,6 @@ void client_focus_update(client_t *);
 void client_unfocus(client_t *);
 void client_unfocus_update(client_t *);
 bool client_hasproto(client_t *, xcb_atom_t);
-void client_set_focus(client_t *, bool);
 void client_ignore_enterleave_events(void);
 void client_restore_enterleave_events(void);
 void client_class_setup(lua_State *);
