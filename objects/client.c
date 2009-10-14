445ae2c0a47bb6bf418930219eef2a73fe4dfcaf
Julien Danjou <julien@danjou.info>
2009-10-14 17:53:23 +0200

    client: remove a bad push
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- objects/client.c
+++ objects/client.c
@@ -258,7 +258,6 @@ client_focus_update(client_t *c)
     /* according to EWMH, we have to remove the urgent state from a client */
     client_set_urgent(globalconf.L, -1, false);
 
-    luaA_object_push(globalconf.L, c);
     luaA_object_emit_signal(globalconf.L, -1, "focus", 0);
     lua_pop(globalconf.L, 1);
 }
