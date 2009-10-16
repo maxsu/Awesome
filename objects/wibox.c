217e8b134a1da9725b023d41768b427f8e6c0db3
Julien Danjou <julien@danjou.info>
2009-10-16 21:20:42 +0200

    window: emit key::{press,release} events
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- objects/wibox.c
+++ objects/wibox.c
@@ -195,6 +195,7 @@ wibox_init(wibox_t *w, int phys_screen)
                           w->border_color.pixel,
                           1,
                           XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT
+                          | XCB_EVENT_MASK_KEY_PRESS | XCB_EVENT_MASK_KEY_RELEASE
                           | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_ENTER_WINDOW
                           | XCB_EVENT_MASK_LEAVE_WINDOW | XCB_EVENT_MASK_STRUCTURE_NOTIFY
                           | XCB_EVENT_MASK_POINTER_MOTION | XCB_EVENT_MASK_EXPOSURE
