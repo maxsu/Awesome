04403d932555f0fb51889a81ab6fef2c8f154cf4
Julien Danjou <julien@danjou.info>
2009-10-17 22:45:01 +0200

    xwindow: grab in ASYNC MODE
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- awesome.c
+++ awesome.c
@@ -527,8 +527,6 @@ main(int argc, char **argv)
                 | XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_LEAVE_WINDOW
                 | XCB_EVENT_MASK_STRUCTURE_NOTIFY
                 | XCB_EVENT_MASK_PROPERTY_CHANGE
-                | XCB_EVENT_MASK_BUTTON_PRESS
-                | XCB_EVENT_MASK_BUTTON_RELEASE
                 | XCB_EVENT_MASK_FOCUS_CHANGE
         };
 
