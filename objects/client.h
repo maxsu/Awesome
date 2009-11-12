9132f0a2a35bc9756e9b6d8a983d0436b2bc542c
Julien Danjou <julien@danjou.info>
2009-11-12 11:31:49 +0100

    client: client_geometry_hints is static
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- objects/client.h
+++ objects/client.h
@@ -102,7 +102,6 @@ client_t * client_getbywin(xcb_window_t);
 client_t * client_getbyframewin(xcb_window_t);
 
 void client_manage(xcb_window_t, xcb_get_geometry_reply_t *, bool);
-area_t client_geometry_hints(client_t *, area_t);
 bool client_resize(client_t *, area_t);
 void client_unmanage(client_t *);
 void client_kill(client_t *);
