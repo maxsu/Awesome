17fbf12b812ed9c042f8de7ca0f159baa0eee3ff
Julien Danjou <julien@danjou.info>
2009-11-23 17:57:56 +0100

    root window: listen to all mouse events
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- awesome.c
+++ awesome.c
@@ -472,6 +472,9 @@ main(int argc, char **argv)
                                  | XCB_EVENT_MASK_STRUCTURE_NOTIFY
                                  | XCB_EVENT_MASK_PROPERTY_CHANGE
                                  | XCB_EVENT_MASK_FOCUS_CHANGE
+                                 | XCB_EVENT_MASK_BUTTON_PRESS
+                                 | XCB_EVENT_MASK_BUTTON_RELEASE
+                                 | XCB_EVENT_MASK_POINTER_MOTION
                                  });
     systray_init();
     ewmh_init();
