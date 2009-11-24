578cd17ce7a29ad7681267052b1e79e4d5314de9
Julien Danjou <julien@danjou.info>
2009-11-24 22:00:23 +0100

    luaclass: remove useless luaA_openlib
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- common/luaclass.h
+++ common/luaclass.h
@@ -83,7 +83,6 @@ void luaA_class_connect_signal_from_stack(lua_State *, lua_class_t *, const char
 void luaA_class_disconnect_signal_from_stack(lua_State *, lua_class_t *, const char *, int);
 void luaA_class_emit_signal(lua_State *, lua_class_t *, const char *, int);
 
-void luaA_openlib(lua_State *, const char *, const struct luaL_reg[], const struct luaL_reg[]);
 void luaA_class_setup(lua_State *, lua_class_t *, const char *, lua_class_t *, size_t,
                       lua_class_initializer_t, lua_class_collector_t,
                       lua_class_checker_t,
